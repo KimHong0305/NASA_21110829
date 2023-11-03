525043541f97d3105c5fd97af487942f6226191a
KimHong0305 <nguyenkimhong0305@gmail.com>
2023-11-03 23:53:37 +0700

    First commit

---
+++ server/src/routes/phanets/planets.controller.js
@@ -0,0 +1,10 @@
+const { planets } = require('../../models/planets.model');
+
+
+function httpGetAllPlanets(req, res) {
+    return res.status(200).json(getAllPlanets());
+}
+
+module.exports = {
+    httpGetAllPlanets,
+};
